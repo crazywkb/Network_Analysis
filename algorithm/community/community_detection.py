4e5b3921ccb7167368eea87cb9edc1be064a9a65
crazywkb <crazyk.wkb@gmail.com>
2018-04-15 01:55:16 +0800

    optimize function: count_two_dimension()

--- algorithm/community/community_detection.py
+++ algorithm/community/community_detection.py
@@ -1,10 +1,18 @@
 from community.community_louvain import best_partition
 from networkx.algorithms.community import girvan_newman
+from collections import defaultdict
 
 
-def louvain(graph):
-    best_partition(graph)
-    pass
+def louvain(graph, partition=None, randomize=False):
+    partition_result = best_partition(graph, partition=partition, randomize=randomize)
+    partition_dict_keys = set(partition_result.values())
+    partition_dict = defaultdict(list)
+    partition_dict.fromkeys(partition_dict_keys)
+
+    for node, part_num in partition_result.items():
+        partition_dict[part_num].append(node)
+
+    return partition_dict
 
 
 def fast_newman(graph, part_sum):
--- utils/count_entropy.py
+++ utils/count_entropy.py
@@ -1,5 +1,6 @@
 from math import log
 
+from algorithm.community.community_detection import louvain
 import networkx as nx
 
 
@@ -14,48 +15,47 @@ def count_one_dimension(graph):
 
     for node, node_degree in graph.degree:
         temp = node_degree / total_degree
-        entropy += temp * log(temp, 2)
+        entropy -= temp * log(temp, 2)
 
     return entropy
 
 
-def count_two_dimension(graph, parts_list):
+def count_two_dimension(graph, parts_dict):
     """
 
     :param graph: networkx.classes.graph.Graph
-    :param parts_list: list
+    :param parts_dict: dict of parts, (key, value): (part_num, part_list)
     :return: structure entropy of two dimension
     """
-    parts_sum = len(parts_list)
     total_degree = len(graph.edges)
+    assert total_degree != 0
 
     # first part: count information of nodes in its own module
     # second part: count information of the module that is accessible from random walk from nodes outside the module
     first_part = 0
     second_part = 0
-    for partition in parts_list:
+
+    for partition in parts_dict.values():
         part_capacity = len(partition)
         part_entropy = 0
+        sub_graph = graph.subgraph(partition)
+        partition_degree_global = 0
+        partition_degree_sub = 0
 
         for node in partition:
             node_degree = graph.degree[node]
             temp = node_degree / part_capacity
             part_entropy += temp * log(temp, 2)
 
+            partition_degree_global += node_degree
+            partition_degree_sub += sub_graph.degree[node]
+
+        cross_edges_sum = partition_degree_global - partition_degree_sub
+
         first_part -= (part_capacity / total_degree) * part_entropy
+        second_part -= cross_edges_sum / total_degree * log(part_capacity / total_degree, 2)
 
-        sub_graph = graph.subgraph(partition)
-        diff_edges = graph.edges - sub_graph.edges
-        diff_edges_sum = 0
-
-        for src, des in diff_edges:
-            if src in partition or des in partition:
-                diff_edges += 1
-
-        second_part += diff_edges_sum / total_degree * log(part_capacity)
-
-
-    return first_part - second_part
+    return first_part + second_part
 
 
 def count_multiple_dimension(graph, parts_list):
@@ -66,6 +66,7 @@ def count_multiple_dimension(graph, parts_list):
     :return:
     """
     pass
+    return 0
 
 
 def count_volume(graph, module):
@@ -81,3 +82,5 @@ def count_volume(graph, module):
 if __name__ == '__main__':
     test_graph = nx.karate_club_graph()
     print(count_one_dimension(test_graph))
+    partition_dict = louvain(test_graph)
+    print(count_two_dimension(test_graph, partition_dict))
