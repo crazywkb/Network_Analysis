a75ef33463e753d38678d2d2177358d1526c70ed
crazywkb <crazyk.wkb@gmail.com>
2018-04-16 01:38:28 +0800

    finish counting one and two dimensional structure entropy, and change the output style.

--- algorithm/community/community_detection.py
+++ algorithm/community/community_detection.py
@@ -1,13 +1,24 @@
+from collections import defaultdict
+
 from community.community_louvain import best_partition
 from networkx.algorithms.community import girvan_newman
-from collections import defaultdict
 
 
 def louvain(graph, partition=None, randomize=False):
+    """
+    warp the best_partition, change the result into a dict
+    Compute the partition of the graph nodes which maximises the modularity
+    (or try..) using the Louvain heuristices
+
+    :param graph: networkx.classes.graph.Graph
+    :param partition: dict, optional.
+    :param randomize: boolean, optional
+    :return: dict, the result of partition, {module_num: [module]}
+    """
     partition_result = best_partition(graph, partition=partition, randomize=randomize)
     partition_dict_keys = set(partition_result.values())
     partition_dict = defaultdict(list)
-    partition_dict.fromkeys(partition_dict_keys)
+    partition_dict.fromkeys(list(partition_dict_keys))
 
     for node, part_num in partition_result.items():
         partition_dict[part_num].append(node)
@@ -16,7 +27,8 @@ def louvain(graph, partition=None, randomize=False):
 
 
 def fast_newman(graph, part_sum):
-    girvan_newman(graph, part_sum)
+    girvan_newman(graph)
+    part_sum.test()
     pass
 
 
--- utils/count_entropy.py
+++ utils/count_entropy.py
@@ -1,10 +1,7 @@
 from math import log
 
-from algorithm.community.community_detection import louvain
-import networkx as nx
 
-
-def count_one_dimension(graph):
+def count_one_dimension(graph, printed=True):
     """
     count graph's entropy of one dimension
     :param graph: networkx.classes.graph.Graph
@@ -17,14 +14,20 @@ def count_one_dimension(graph):
         temp = node_degree / total_degree
         entropy -= temp * log(temp, 2)
 
+    if printed:
+        print("=========================================================================================================")
+        print("The one-dimensional structure entropy of graph %s: %f"%(graph, entropy))
+        print()
+
     return entropy
 
 
-def count_two_dimension(graph, parts_dict):
+def count_two_dimension(graph, modules_dict, printed=True):
     """
-
+    count two-dimensional structure entropy
+    :param printed: boolean, print the result or not
     :param graph: networkx.classes.graph.Graph
-    :param parts_dict: dict of parts, (key, value): (part_num, part_list)
+    :param modules_dict: dict of parts, (key, value): (module_num, modules_list)
     :return: structure entropy of two dimension
     """
     total_degree = len(graph.edges)
@@ -35,36 +38,46 @@ def count_two_dimension(graph, parts_dict):
     first_part = 0
     second_part = 0
 
-    for partition in parts_dict.values():
-        part_capacity = len(partition)
-        part_entropy = 0
-        sub_graph = graph.subgraph(partition)
-        partition_degree_global = 0
-        partition_degree_sub = 0
+    for module in modules_dict.values():
+        module_capacity = len(module)
+        module_entropy = 0
+        sub_module_graph = graph.subgraph(module)
+        module_degree = 0  # the sum of nodes' degree in module
+        sub_module_degree = 0  # the sum of nodes' degree in sub_module
 
-        for node in partition:
+        for node in module:
             node_degree = graph.degree[node]
-            temp = node_degree / part_capacity
-            part_entropy += temp * log(temp, 2)
+            temp = node_degree / module_capacity
+            module_entropy += temp * log(temp, 2)
 
-            partition_degree_global += node_degree
-            partition_degree_sub += sub_graph.degree[node]
+            module_degree += node_degree
+            sub_module_degree += sub_module_graph.degree[node]
 
-        cross_edges_sum = partition_degree_global - partition_degree_sub
+        cross_edges_sum = module_degree - sub_module_degree
 
-        first_part -= (part_capacity / total_degree) * part_entropy
-        second_part -= cross_edges_sum / total_degree * log(part_capacity / total_degree, 2)
+        first_part -= (module_capacity / total_degree) * module_entropy
+        second_part -= cross_edges_sum / total_degree * log(module_capacity / total_degree, 2)
 
-    return first_part + second_part
+    entropy = first_part + second_part
+    if printed:
+        print("=========================================================================================================")
+        print("The two-dimensional structure entropy of graph %s: %f"%(graph, entropy))
+        print("The modules' dict: ")
+        for module_num, module in modules_dict.items():
+            print("%-4s"%module_num, module)
+        print()
+
+    return entropy
 
 
-def count_multiple_dimension(graph, parts_list):
+def count_multiple_dimension(graph, parts_list, printed=True):
     """
     There may be a problem.
     :param graph:
     :param parts_list:
     :return:
     """
+    # TODO: count multiple dimension structure entropy.
     pass
     return 0
 
@@ -80,7 +93,10 @@ def count_volume(graph, module):
 
 
 if __name__ == '__main__':
+    from algorithm.community.community_detection import louvain
+    import networkx as nx
+
     test_graph = nx.karate_club_graph()
-    print(count_one_dimension(test_graph))
-    partition_dict = louvain(test_graph)
-    print(count_two_dimension(test_graph, partition_dict))
+    result_dict = louvain(test_graph)
+    (count_one_dimension(test_graph))
+    (count_two_dimension(test_graph, result_dict))
