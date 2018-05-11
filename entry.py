2a805369f47274d38d31ca9ff6bf393dbb881234
crazywkb <crazyk.wkb@gmail.com>
2018-05-11 15:10:10 +0800

    add logging modules

--- entry.py
+++ entry.py
@@ -1,11 +1,21 @@
 # coding=utf-8
 import os
+from simple_settings import settings
+import logging.config
+
+
+def initial():
+    os.environ['SIMPLE_SETTINGS'] = 'settings.' + os.sys.argv[1]
+    settings.setup()
+    logging.config.dictConfig(settings.LOGGING_CONFIG)
+
+
+def get_logger(name):
+    assert isinstance(name, str)
+    return logging.getLogger(name)
+
 
 if __name__ == '__main__':
-    os.environ['SIMPLE_SETTINGS'] = 'settings.' + os.sys.argv[1]
-    from simple_settings import settings
-
-    settings.setup()
-
-    print(settings.as_dict())
-    print(settings.UPDATE_INTERVAL)
+    initial()
+    logger = get_logger("test")
+    logger.info("This is not a test.")
